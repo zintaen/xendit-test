24d2d8c407e04aafa41ec61c45f670ac00942143
Stephen Cheng <zintaen@gmail.com>
2021-03-04 10:49:45 +0700

    fix: use env

--- utils/api.ts
+++ utils/api.ts
@@ -1,16 +1,9 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000';
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
-export const authApi = axios.create({
-  baseURL,
-  timeout: 3000,
-});
-
-const api = axios.create({
-  baseURL,
-  timeout: 3000,
-});
+export const authApi = axios.create({ baseURL });
+const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
   const token = window.localStorage.getItem('access_token');
